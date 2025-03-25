de8e37f8c245d625943412ea1504831e9621435d
Balint Szikora <chamaselion@gmail.com>
2025-03-25 13:37:45 +0100

    Added initlist to constructor so the name is always initialized

--- ex00/sources/Zombie.cpp
+++ ex00/sources/Zombie.cpp
@@ -6,13 +6,13 @@
 /*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 12:33:09 by bszikora          #+#    #+#             */
-/*   Updated: 2025/03/25 13:09:58 by bszikora         ###   ########.fr       */
+/*   Updated: 2025/03/25 13:37:16 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Zombie.hpp>
 
-Zombie::Zombie() { };
+Zombie::Zombie() : name("Zombie") {}
 Zombie::~Zombie() { std::cout << this->name << " has been evaporated" << std::endl; }
 Zombie::Zombie(std::string name) { this->name = name; }
-void Zombie::announce( void ) {std::cout << this->name << ":  BraiiiiiiinnnzzzZ...\n";}
+void Zombie::announce( void ) {std::cout << this->name << ": BraiiiiiiinnnzzzZ...\n";}
