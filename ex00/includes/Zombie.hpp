5e3e60cda287fe6da86d8fcef965f9a9f95f2486
Balint Szikora <chamaselion@gmail.com>
2025-03-25 12:37:00 +0100

    Created zombie class and implemented announce function

---
+++ ex00/includes/Zombie.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 12:24:36 by bszikora          #+#    #+#             */
+/*   Updated: 2025/03/25 12:32:36 by bszikora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef ZOMBIES_HPP
+# define ZOMBIES_HPP
+
+#include <iostream>
+
+class Zombie
+{
+private:
+	std::string name;
+	
+public:
+	Zombie*	newZombie( std::string name );
+	void 	randomChump( std::string name );
+	void	announce( void );
+
+};
+
+# endif
