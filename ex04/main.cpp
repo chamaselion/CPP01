85b7c5d86994ad4498835066db0c973183fc7f99
Balint Szikora <chamaselion@gmail.com>
2025-05-05 17:28:39 +0200

    Added protection for non-existent, no rights file handling

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 12:48:07 by bszikora          #+#    #+#             */
-/*   Updated: 2025/03/27 12:14:31 by bszikora         ###   ########.fr       */
+/*   Updated: 2025/05/04 14:26:02 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,38 @@
 
 void replace_in_file(const std::string& input_file, const std::string& output_file, const std::string& s1, const std::string& s2)
 {
-    std::ifstream infile(input_file.c_str());
-    std::ofstream outfile(output_file.c_str());
-    std::string line;
-    std::string modified_line;
-    size_t start_pos;
-    size_t found_pos;
+	std::string	line;
+    std::string	modified_line;
+    size_t		start_pos;
+    size_t		found_pos;
 
-    if (!infile || !outfile)
-    {
-        std::cerr << "Error opening file!\n";
-        return;
-    }
-    while (std::getline(infile, line))
-    {
-        modified_line.clear();
-        start_pos = 0;
-        while ((found_pos = line.find(s1, start_pos)) != std::string::npos)
-        {
-            modified_line.append(line, start_pos, found_pos - start_pos);
-            modified_line.append(s2);
-            start_pos = found_pos + s1.length();
-        }
+	if (s1.empty())
+	{
+		std::cerr << "Error: search string cannot be empty\n";
+		return;
+	}
+    std::ifstream infile(input_file.c_str());
+	if (!infile)
+	{
+		std::cerr << "Error opening input file!\n";
+		return;
+	}
+	std::ofstream outfile(output_file.c_str());
+	if (!outfile)
+	{
+		std::cerr << "Error creating output file!\n";
+		return;
+	}
+	while (std::getline(infile, line))
+	{
+		modified_line.clear();
+		start_pos = 0;
+		while ((found_pos = line.find(s1, start_pos)) != std::string::npos)
+		{
+			modified_line.append(line, start_pos, found_pos - start_pos);
+			modified_line.append(s2);
+			start_pos = found_pos + s1.length();
+		}
         modified_line.append(line, start_pos, line.length() - start_pos);
         outfile << modified_line << '\n';
     }
